c2a0c24a3b1b1d2203b2755346313d5b6ccc9207
xiangyi <xiangyi-y@qq.com>
2020-07-08 14:18:31 +0800

    第一次提交

---
+++ src/pages/Edu/Teacher/components/Test/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>test...</h1>
+            </div>
+        )
+    }
+}

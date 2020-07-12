3f7b674866b06e0ff3710cd06a9f41577665d6ba
xiangyi <xiangyi-y@qq.com>
2020-07-12 19:10:24 +0800

    新增了课程分类添加按钮

--- src/pages/Edu/Subject/index.jsx
+++ src/pages/Edu/Subject/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-
+import {Button, Card, Input} from 'antd'
 export default class Subject extends Component {
   render() {
     return <div>
-      测试Subject...
-    </div>;
+     <Button>添加</Button>
+     <Card></Card>
+    </div>
   }
 }

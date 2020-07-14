a57d49c9d17198ccf891e50984853354b761ba63
xiangyi <xiangyi-y@qq.com>
2020-07-14 23:52:55 +0800

    课程结构新建

--- src/pages/Edu/Subject/index.jsx
+++ src/pages/Edu/Subject/index.jsx
@@ -1,7 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import {Button, Table} from 'antd'
+import {PlusOutlined} from '@ant-design/icons'
 
+import './index.less'
 export default class Subject extends Component {
   render() {
-    return <div>  Subject... </div>
+    return (
+      <div className= 'subject'>
+        <Button type='primary' className='subject-btn' onClick={this.handleGoAddSubject}>
+          <PlusOutlined />
+          新建
+        </Button>
+
+        <Table>
+            
+        </Table>
+
+        
+      </div>
+    )
   }
 }
+

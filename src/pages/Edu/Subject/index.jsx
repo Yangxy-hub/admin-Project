b8f8be5abb18b36ad370e36e8babb3bbbb66711a
xiangyi <xiangyi-y@qq.com>
2020-07-14 08:37:05 +0800

    新增subject

--- src/pages/Edu/Subject/index.jsx
+++ src/pages/Edu/Subject/index.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
-import {Button, Card, Input} from 'antd'
+
 export default class Subject extends Component {
   render() {
-    return <div>
-     <Button>添加</Button>
-     <Card></Card>
-    </div>
+    return <div>  Subject... </div>
   }
 }
